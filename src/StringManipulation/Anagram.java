30db2c3ccc84ce1f638af525e64d1d6f5d3714f8
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-07-24 20:10:00 +0200

    update Anagram code to make use of utility function

--- src/StringManipulation/Anagram.java
+++ src/StringManipulation/Anagram.java
@@ -1,11 +1,16 @@
 package StringManipulation;
 
 import java.util.Arrays;
+import java.util.List;
+
+import QuickUtils.StringUtils;
 
 public class Anagram {
     public String anagramChecker(String wordInit, String targetInit){
-        String word = wordInit.replaceAll("\\s", "");
-        String target = targetInit.replaceAll("\\s", "");
+        StringUtils stripper = new StringUtils();
+        List<String> wordList = stripper.StripSpaces(wordInit, targetInit);
+        String word = wordList.get(0);
+        String target = wordList.get(1);
 
         String notAnagram = targetInit + " is not an anagram of " + wordInit,
                 isAnagram = targetInit + " is an anagram of " + wordInit;
