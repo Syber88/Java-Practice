9b871438e3f11cedce4ae40bd3cf323dbf979463
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-07-24 13:28:52 +0200

    modified code as .ToList function was returning immutable list

--- src/StringManipulation/WordReversal.java
+++ src/StringManipulation/WordReversal.java
@@ -1,21 +1,21 @@
 package StringManipulation;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class WordReversal {
     public void reverse(String words){
         String reversedWord = "";
         String[] arrayStr =  words.split(" ");
-        Collections.reverse(Arrays.stream(arrayStr).toList());
-        for (String word : arrayStr){
-            reversedWord += word;
-            System.out.println(reversedWord);
+        List<String> mutableList = new ArrayList<>(Arrays.asList(arrayStr));
+        Collections.reverse(mutableList);
+        for (String word : mutableList){
+            reversedWord += word + " ";
         }
+        System.out.println(reversedWord);
 //        return reversedWord;
     }
 }
 
-
-
-131483083
