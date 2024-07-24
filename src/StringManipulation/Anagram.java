bb7c06b2f023c3f0219b29c2b91801edcee0f995
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-07-24 16:18:17 +0200

    anagram checker now complete

--- src/StringManipulation/Anagram.java
+++ src/StringManipulation/Anagram.java
@@ -4,19 +4,18 @@ import java.util.Arrays;
 
 public class Anagram {
     public String anagramChecker(String word, String target){
-        String notAnagram = word + "is  not an anagram of " + target;
-
+        String notAnagram = word + "is not an anagram of " + target,
+                isAnagram = word + "is an anagram of " + target;
         if (word.length() == target.length()){
             char[] tempArray = word.toCharArray(),targetArray = target.toCharArray();
             Arrays.sort(tempArray);
             Arrays.sort(targetArray);
 
-            for (int i = 0;i < word.length();i++){
-                int left = i, right = word.length();
+            for (int i = 0; i < word.length(); i++){
+                if (tempArray[i] != targetArray[i])
+                    return notAnagram;
             }
-        } else{
-            return notAnagram;
         }
-
+        return isAnagram;
     }
 }
