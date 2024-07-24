8b4f524aaa95113cb8d637a80f49d66e6c53bc3e
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-07-24 22:55:54 +0200

    added java docs for the functions i created

--- src/StringManipulation/Pangram.java
+++ src/StringManipulation/Pangram.java
@@ -2,8 +2,6 @@ package StringManipulation;
 
 import QuickUtils.StringUtils;
 
-import java.util.List;
-
 public class Pangram {
     private boolean perfectPangram = false;
 
@@ -11,16 +9,10 @@ public class Pangram {
         if (word.length() == 26) this.perfectPangram = true;
         String characters = "";
         for (char letter : word.toCharArray()){
-            if (!containsChar(word.toCharArray(), letter))
+            StringUtils charCon = new StringUtils();
+            if (!charCon.containsChar(word.toCharArray(), letter))
                 characters += letter;
 
         }
-    }
-    public boolean containsChar(char[] characters, char letter){
-        for (char letters : characters){
-            if (letters == letter)
-                return true;
-        }
-        return false;
     }
 }
