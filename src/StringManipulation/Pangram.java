fa507a1e4f5219f4f038ab732ab846559dc1d130
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-07-24 23:19:56 +0200

    completed pangram but still need to do further tests and add error checking

--- src/StringManipulation/Pangram.java
+++ src/StringManipulation/Pangram.java
@@ -5,14 +5,21 @@ import QuickUtils.StringUtils;
 public class Pangram {
     private boolean perfectPangram = false;
 
-    public void PangramChecker(String word){
+    public String PangramChecker(String word){
+
+        String Affirmative = word + " is a Pangram ";
+        String Negative = word + " is a not Pangram ";
+
         if (word.length() == 26) this.perfectPangram = true;
-        String characters = "";
+        StringBuilder characters = new StringBuilder();
+
         for (char letter : word.toCharArray()){
             StringUtils charCon = new StringUtils();
             if (!charCon.containsChar(word.toCharArray(), letter))
-                characters += letter;
+                characters.append(letter);
 
+            if (characters.length() == 26) return Affirmative;
         }
+        return Negative;
     }
 }
