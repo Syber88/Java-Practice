797bc3e2246bc0d023c0595bcf933e8bd251d19b
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-07-24 22:13:58 +0200

    partially completed with pangram, i need to add a utility function for sorting letters

--- src/StringManipulation/Pangram.java
+++ src/StringManipulation/Pangram.java
@@ -1,10 +1,26 @@
 package StringManipulation;
 
-public class Pangram {
-    boolean perfectPangram = false;
-    public String PangramChecker(String word){
-        if (word.length() == 26)
-        this.perfectPangram = true;
+import QuickUtils.StringUtils;
 
+import java.util.List;
+
+public class Pangram {
+    private boolean perfectPangram = false;
+
+    public void PangramChecker(String word){
+        if (word.length() == 26) this.perfectPangram = true;
+        String characters = "";
+        for (char letter : word.toCharArray()){
+            if (!containsChar(word.toCharArray(), letter))
+                characters += letter;
+
+        }
+    }
+    public boolean containsChar(char[] characters, char letter){
+        for (char letters : characters){
+            if (letters == letter)
+                return true;
+        }
+        return false
     }
 }
