19e2498bcdf939dd62233064158b72b675ad2c3a
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-08-25 00:17:16 +0200

    context

--- src/Numbers/NumberUtils.java
+++ src/Numbers/NumberUtils.java
@@ -5,6 +5,12 @@ import java.util.*;
 
 public class NumberUtils {
 
+    /*Given n, take the sum of the digits of n. If that value has more than one digit,
+    continue reducing in this way until a single-digit number is produced. The input
+    will be a non-negative integer.
+    Examples
+    16  -->  1 + 6 = 7
+    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6*/
     public static int digitalRoot(int number){
         if (number < 9){
             return number;
